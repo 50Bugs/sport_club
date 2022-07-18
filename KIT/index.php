6220d4b1e6251af5740f9161137ca65d403ea7eb
50Bugs <85785219+50Bugs@users.noreply.github.com>
2022-07-18 22:20:36 +0300

    Update index.php

--- KIT/index.php
+++ KIT/index.php
@@ -146,7 +146,7 @@
             </div>
             <div class="carousel-inner">
                 <div class="carousel-item active" data-bs-interval="10000">
-                    <img src="src/pexels-mike-1192043 (1).jpg" class="d-block w-100" alt="...">
+                    <img src="src/pexels-mike-1192043 (1).jpg.jpg" class="d-block w-100" alt="...">
                     <div class="carousel-caption d-none d-md-block">
                         <h5>Здоровий спосіб життя</h5>
                         <p>Уникніть захворювань пов'язаних з сидячим способом життя</p>
